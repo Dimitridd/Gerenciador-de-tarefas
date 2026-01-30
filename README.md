c708181313d5ea5f3749f7e390c69cf97bd8de5e
DimiDD <dimitri.d.deccache@gmail.com>
2026-01-30 19:18:54 -0300

    Atualização. Vou reutiliar essa pasta de git configurada para fazer o novo projeto de avaliação/comparação do preço do dólar

---
+++ README.md
@@ -0,0 +1,7 @@
+Monitor de Câmbio em Tempo Real:
+
+Conexão: O programa se conecta a uma API financeira real para buscar o preço mais recente.
+Memória: Ele lê um arquivo historico.json para saber qual era o preço da última vez que ele verificou.
+Inteligência (Lógica): Ele compara o preço novo com o antigo e decide se o dólar subiu, caiu ou estabilizou.
+Automação: Em vez de você rodar o script toda hora, ele fica rodando sozinho em um loop infinito, verificando o preço a cada 30 segundos (ou o tempo que você definir).
+Registro: Ele salva o novo preço no arquivo para a próxima comparação.
---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import time
+
+url = "https://economia.awesomeapi.com.br/last/USD-BRL"
+
+def main():
+    resposta = requests.get(url)
+    dados = resposta.json()
+
+    def salvar_preco(preco):
+        dados_para_salvar = {'ultimo_preco': preco}
+        with open('historico.json', 'w') as arquivo:
+            json.dump(dados_para_salvar, arquivo)
+
+    try:
+        with open('historico.json', 'r') as arquivo:
+            valor_lido = json.load(arquivo)
+        valor_referencia = valor_lido['ultimo_preco']
+    except:
+        valor_referencia = 0
+
+    valor_atual = float(dados['USDBRL']['bid'])
+
+    print(f"O valor do dólar é : {valor_atual}")
+    print(f"O valor antigo do dólar é : {valor_referencia}")
+
+    if valor_referencia == 0:
+        print("Este é a primeira análise")
+    elif valor_atual > valor_referencia:
+        print("O dólar subiu")
+    elif valor_atual == valor_referencia:
+        print("O dólar está estável")
+    else:
+        print("O dólar caiu")
+
+    salvar_preco(valor_atual)
+
+if __name__ == "__main__":
+    while True:
+        main()
+        print("Aguardando 30 segundos para a próxima verificação...")
+        time.sleep(30) # Faz o programa "dormir" por 30 segundos
